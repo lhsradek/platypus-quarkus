8a7b47b5ea899112430b154a6e52e0e6339023fb
Radek Kádner <radek.kadner@gmail.com>
2023-03-11 10:35:17 +0100

    .

--- src/test/java/local/intranet/quarkus/NazdarResourceIT.java
+++
@@ -1,8 +0,0 @@
-package local.intranet.quarkus;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class NazdarResourceIT extends HelloResourceTest {
-    // Execute the same tests but in packaged mode.
-}
