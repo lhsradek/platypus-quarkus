8de82e74f5ef6789af76c7bde5f315958f0995a4
Radek Kadner <radek.kadner@gmail.com>
2023-04-23 02:46:19 +0200

    .

---
+++ src/test/java/local/intranet/quarkus/ConversionTest.java
@@ -0,0 +1,33 @@
+package local.intranet.quarkus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+import local.intranet.quarkus.api.util.SecurityUtil;
+
+@QuarkusTest
+@TestProfile(CustomProfile.class)
+public class ConversionTest {
+
+	@Test
+	public void testConversion() {
+		
+		// a bit of Dadaism
+		
+		assertEquals("We have a stuffed grandfather in the closet.", SecurityUtil
+				.getBase64("V2UgaGF2ZSBhIHN0dWZmZWQgZ3JhbmRmYXRoZXIgaW4gdGhlIGNsb3NldC4="));
+		
+		assertEquals("V2UgaGF2ZSBhIHN0dWZmZWQgZ3JhbmRmYXRoZXIgaW4gdGhlIGNsb3NldC4=", SecurityUtil
+				.setBase64("We have a stuffed grandfather in the closet."));
+		
+		assertEquals("My cork badtub is like your giraffe rye!", SecurityUtil
+				.getHex("4d7920636f726b20626164747562206973206c696b6520796f757220676972616666652072796521"));
+		
+		assertEquals("4d7920636f726b20626164747562206973206c696b6520796f757220676972616666652072796521", SecurityUtil
+		 		.setHex("My cork badtub is like your giraffe rye!"));
+	}
+
+}
